d04d2d368eed6e6a146dc8a555fed86cf1b51331
钟俊滨 <rekadowney@163.com>
2018-06-27 23:27:33 +0800

    检出configuration分支的AppConstant类

---
+++ src/main/java/me/junbin/learning/springboot/constant/AppConstant.java
@@ -0,0 +1,16 @@
+package me.junbin.learning.springboot.constant;
+
+/**
+ * @author : Zhong Junbin
+ * @email : <a href="mailto:rekadowney@gmail.com">发送邮件</a>
+ * @createDate : 2018/6/24 22:39
+ * @description :
+ */
+public class AppConstant {
+
+    public static class Profiles {
+        public static final String DEV = "dev";
+        public static final String PROD = "prod";
+    }
+
+}
